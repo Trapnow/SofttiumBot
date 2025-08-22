835277561004d68b1fa3b92d0e777ebd2fd203c2
Данил <danil.arhishin@mail.ru>
2025-08-22 12:03:46 +0300

    add

--- SoftiumBot/routers/handlers/commands.py
+++ SoftiumBot/routers/handlers/commands.py
@@ -1,7 +1,7 @@
 from aiogram.types import Message
 from aiogram import Router
 
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 
 from aiogram.fsm.context import FSMContext
 
@@ -14,3 +14,8 @@ router = Router(name=__name__)
 async def cmd_start(msg: Message, state: FSMContext):
     await state.clear()
     await msg.answer("Добро пожаловать в школу программирования Софтиум!", reply_markup=main_menu_ikb())
+
+
+@router.message(Command("description"))
+async def cmd_start(msg: Message):
+    await msg.answer("Описание бота")
