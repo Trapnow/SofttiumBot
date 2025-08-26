2bec2a964a4302cd5706ee96e4f9a7373c53aed8
Данил <danil.arhishin@mail.ru>
2025-08-26 15:14:42 +0300

    add

--- SoftiumBot/keyboards/inline_keyboards.py
+++ SoftiumBot/keyboards/inline_keyboards.py
@@ -5,7 +5,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 def main_menu_ikb() -> InlineKeyboardMarkup:
     markup = InlineKeyboardBuilder()
     markup.add(InlineKeyboardButton(text="Презентация о школе",
-                                    url="https://disk.yandex.ru/i/o0AsHpKhDQnFqw"),
+                                    url="https://drive.google.com/file/d/1Glsu-J9AQkYE9MokNjn1t616K64y6zoV/view?usp=drive_link"),
                InlineKeyboardButton(text="Наш ТГ-канал", callback_data="tg_chanel"),
                InlineKeyboardButton(text="Подобрать программу для ребёнка", callback_data="program"),
                InlineKeyboardButton(text="Запись на пробное занятие", callback_data="trial_session"),
